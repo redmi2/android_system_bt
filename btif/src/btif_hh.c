aed6e2de16442730dd63980579875a52d20160f9
Hemant Gupta <hemantg@codeaurora.org>
2015-12-10 22:25:16 +0530

    HID: Remove bond for device in case Virtual unplug is ongoing
    Remove bond for remote device in case virtual unplug was initiated from
    local device, and ACL got disconnected while processing the VUP request.
    this can happen when aaplication is sending disconnect and remove bond
    in quick succession. Without this fix it was observed that device was
    getting disconnected, but was not getting unpaired even when user
    tried to unpair from UI.
    
    Change-Id: Ife1fc99ed59b13daf0bcf0dce3e0a59c7d9e87d7
    CRs-Fixed: 950036

--- btif/src/btif_hh.c
+++ btif/src/btif_hh.c
@@ -883,6 +883,15 @@ static void btif_hh_upstreams_evt(UINT16 event, char* p_param)
                     bta_hh_co_destroy(p_dev->fd);
                     p_dev->fd = -1;
                 }
+                BTIF_TRACE_DEBUG("%s: local_vup = %d", __FUNCTION__, p_dev->local_vup);
+                if (p_dev->local_vup)
+                {
+                   /* If it is locally initiated VUP, remove the bond as ACL
+                    * got disconnected while VUP being processed.
+                    */
+                    p_dev->local_vup = FALSE;
+                    BTA_DmRemoveDevice((UINT8 *)p_dev->bd_addr.address);
+                }
                 btif_hh_cb.status = BTIF_HH_DEV_DISCONNECTED;
                 p_dev->dev_status = BTHH_CONN_STATE_DISCONNECTED;
                 HAL_CBACK(bt_hh_callbacks, connection_state_cb,&(p_dev->bd_addr), p_dev->dev_status);
