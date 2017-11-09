2846920226d8a94a913b680e7646230b9f5e8867
rajathr <rajathr@codeaurora.org>
2017-11-09 16:13:14 +0530

    Not start AVDTP connection if peer device is not bonded
    When collission timer is expired, DUT shall not start AVDTP
    connection if peer device is not bonded
    CRs-Fixed: 2124569
    Change-Id: Iea245ad87d678d875ab4bc06f7a39a535fd71920

--- btif/src/btif_av.cc
+++ btif/src/btif_av.cc
@@ -473,6 +473,12 @@ static void btif_av_collission_timer_timeout(UNUSED_ATTR void *data) {
   btif_sm_state_t av_state;
   BD_ADDR av_address;
 
+  if(!btif_storage_is_device_bonded(target_bda)){
+    BTIF_TRACE_IMP("btif_av_collission_timer_timeout: not bonded device ");
+    return;
+  }else{
+    BTIF_TRACE_IMP("btif_av_collission_timer_timeout: bonded device ");
+  }
   bdcpy(av_address, target_bda->address);
   av_state = btif_get_conn_state_of_device(av_address);
   BTIF_TRACE_DEBUG("%s(): AV state: %d", __func__, av_state);
